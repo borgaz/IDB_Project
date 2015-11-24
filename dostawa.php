fd2937a8ef953b3d0e401a35ef4e3ff8ea313612
borgaz <zagrobelny.acn@gmail.com>
2015-11-24 15:46:27 +0100

    nowe podstrony

---
+++ dostawa.php
@@ -0,0 +1,83 @@
+<html>
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-2" />
+	<title>PoZa - sklep z odzieżą dla Ciebie</title>
+	<link href="style.css" rel="stylesheet" type="text/css" /><!--[if IE 5]>
+	<style type="text/css"> 
+	/* place css box model fixes for IE 5* in this conditional comment */
+	#sidebar1 { width: 230px; }
+	</style>
+	<![endif]--><!--[if IE]>
+	<style type="text/css"> 
+	/* place css fixes for all versions of IE in this conditional comment */
+	#sidebar1 { padding-top: 30px; }
+	#mainContent { zoom: 1; }
+	/* the above proprietary zoom property gives IE the hasLayout it needs to avoid several bugs */
+	</style>
+	<![endif]-->
+	</head>
+
+	<body>
+	<!-- begin #container -->
+	<div id="container">
+		<!-- begin #header -->
+		<div id="header">
+			<div class="logoContainer">
+				<div class="logo">
+				</div>
+			</div>
+			<br class="clearfloat" />
+			<div class="sloganContainer">
+				<div class="sloganLeft">
+				</div>
+				<div class="sloganRight">
+				</div>        
+			</div>
+			<div id="navcontainer">
+				<ul id="navlist">
+					<li><a href="http://localhost/index.php" id="current">Home</a></li>
+					<li><a href=http://localhost/damskie.php">Damskie</a></li>
+					<li><a href="http://localhost/meskie.php">Męskie</a></li>
+					<li id="active"><a href="http://localhost/dostawa.php">Dostawa</a></li>
+					<li><a href="http://localhost/kontakt.php">Kontakt</a></li>
+					<a href="http://localhost/koszyk.php">
+						<img src="images/koszyk.png" alt="image preview" width="50" height="50" />
+					</a>
+				</ul>
+			</div>
+		</div>
+		<!-- end #header -->
+		<!-- begin #sidebar1 -->
+		<div id="sidebar1">
+			<img src="images/sidebarTop.gif" alt="image preview" width="202" height="20" />
+			<h3>Damskie</h3>
+			<ul>
+				<li><a href="#">Bluzy</a></li>
+				<li><a href="#">Bluzki</a></li>
+				<li><a href="#">Koszule</a></li>
+				<li><a href="#">Spodnie</a></li>
+			</ul>
+			<h3>Męskie</h3>
+			<ul>
+				<li><a href="#">Bluzy</a></li>
+				<li><a href="#">Bluzki</a></li>
+				<li><a href="#">Koszule</a></li>
+				<li><a href="#">Spodnie</a></li>
+			</ul>
+			<img src="images/sidebarBottom.gif" alt="image preview" width="202" height="20" class="sidebarBottom" />
+		</div>
+		<!-- end #sidebar1 -->
+		<!-- begin #mainContent -->
+		<div id="mainContent">
+		<p>
+			<img src="images/hello.gif" alt="image preview" width="279" height="252" class="floatLeft" />
+			Witaj na stronie naszego sklepu z odzieżą. Cieszymy się, że zaglądasz do nas. Zachęcamy do przejrzenia naszej szerokiej i ciekawej ofery odzieżowej. Mamy nadzieję, że znajdziesz u nas ubranie, jakiego szukasz.
+			</p>
+		</div>
+		<!-- end #mainContent -->
+		<!-- This clearing element should immediately follow the #mainContent div in order to force the #container div to contain all child floats --><br class="clearfloat" />
+	<?php include('stopka.php');?>
+	</div>
+	<!-- end #container -->
+	</body>
+</html>
